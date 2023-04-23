fc02688e45249308a73824bade6de1a4db5ae0d5
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-04-23 15:03:18 +0200

    multi select

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,82 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poke-poke';
+  poke_items = [
+    {
+        "LocalizedName": "Electro",
+        "ColorCode": "#F7D02C",
+        "Name": "Electric"
+        
+    },
+    {
+        "LocalizedName": "Fighting",
+        "ColorCode": "#C22E28",
+        "Name": "Fighting"
+        
+    },
+    {
+        "LocalizedName": "Rock",
+        "ColorCode": "#B6A136",
+        "Name": "Rock"
+    },
+    {
+        "LocalizedName": "Grass",
+        "ColorCode": "#7AC74C",
+        "Name": "Grass"
+    },
+    {
+        "LocalizedName": "Psychic",
+        "ColorCode": "#F95587",
+        "Name": "Psychic"
+    },
+    {
+        "LocalizedName": "Steel",
+        "ColorCode": "#B7B7CE",
+        "Name": "Steel"
+    },
+    {
+        "LocalizedName": "Dark",
+        "ColorCode": "#705746",
+        "Name": "Dark"
+    },
+    {
+        "LocalizedName": "Poison",
+        "ColorCode": "#A33EA1",
+        "Name": "Poison"
+    },
+    {
+        "LocalizedName": "Ghost",
+        "ColorCode": "#735797",
+        "Name": "Ghost"
+    },
+    {
+        "LocalizedName": "Ground",
+        "ColorCode": "#E2BF65",
+        "Name": "Ground"
+    },
+    {
+        "LocalizedName": "Flying",
+        "ColorCode": "#A98FF3",
+        "Name": "Flying"
+    },
+    {
+        "LocalizedName": "Bug",
+        "ColorCode": "#A6B91A",
+        "Name": "Bug"
+    },
+    {
+        "LocalizedName": "Dragon",
+        "ColorCode": "#6F35FC",
+        "Name": "Dragon"
+    }
+];
+  items = [
+    { id: 1, name: 'Item 1', color: '#aaaaaa' },
+    { id: 2, name: 'Item 2', color: '#bbbbbb' },
+    { id: 3, name: 'Item 3', color: '#cccccc' },
+    { id: 4, name: 'Item 4', color: '#dddddd' },
+    { id: 5, name: 'Item 5', color: '#eeeeee' }
+  ];
+
+  selectedItems = [];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +16,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgSelectModule,
+    FormsModule,
+    MatToolbarModule, 
+    MatFormFieldModule, 
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
